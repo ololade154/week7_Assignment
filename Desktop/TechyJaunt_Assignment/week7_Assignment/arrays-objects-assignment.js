5eaf740f9c721f7f65392e35f1a3f908539cb9f3
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-01-08 01:40:59 +0100

    initial commit

---
+++ Desktop/TechyJaunt_Assignment/week7_Assignment/arrays-objects-assignment.js
@@ -0,0 +1,78 @@
+//question one
+//creating of array with object student
+const students = [
+  {
+    id: 1,
+    name: 'Alice Johnson',
+    age: 20,
+    grades: [85, 92, 78],
+  },
+  {
+    id: 2,
+    name: 'shogbaike ololade',
+    age: 20,
+    grades: [75, 80, 90],
+  },
+  {
+    id: 3,
+    name: 'Akintoye Mary',
+    age: 16,
+    grades: [78, 85, 90],
+  },
+  {
+    id: 4,
+    name: 'Lawal Ayomide',
+    age: 23,
+    grades: [60, 70, 69],
+  },
+  {
+    id: 5,
+    name: 'Olanipekun Jerry',
+    age: 30,
+    grades: [39, 40, 58],
+  },
+  {
+    id: 6,
+    name: 'Shobiye Daniel',
+    age: 10,
+    grades: [40, 39, 70],
+  },
+  {
+    id: 7,
+    name: 'Ayorinde Miracle',
+    age: 40,
+    grades: [15, 10, 50],
+  },
+];
+//question two
+//claculating the avegardegrades
+const averageGrades = (grades) => {
+  const sum = grades.reduce((total, grade) => total + grade, 0);
+  const average = sum / grades.length;
+  return Math.round(average * 100) / 100;
+};
+//calculating average for each student
+const studentsWithAverage = students.map((student) => {
+  return {
+    ...student,
+    average: averageGrades(student.grades),
+  };
+});
+console.log(studentsWithAverage);
+//question three
+const getPassingStudents = (students) => {
+  return students.filter((student) => student.average >= 60);
+};
+
+const passing = getPassingStudents(studentsWithAverage);
+console.log(passing);
+//question four
+//calculating class average
+const classAverage = (students) => {
+  const sum = students.reduce((total, student) => total + student.average, 0);
+  const average = sum / students.length;
+  return Math.round(average * 100) / 100;
+};
+
+const classAvg = classAverage(studentsWithAverage);
+console.log(classAvg);
